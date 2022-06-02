62c2e5e1616f21568a5880191c267c50024d5404
Santiago <Martnez Bota>
2022-06-02 18:28:44 +0200

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/personal_portfolio/",
+  base: "/personal_portfolio_dir/",
   plugins: [vue()],
 });
